d0f1cb5d0a62e510d3b05cdb12aa66aa04e2c7b3
ClaySamuelC <claysamuelc@gmail.com>
2020-08-21 13:18:03 -0700

    implement tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -5,6 +5,7 @@ var Tree = function(value) {
   // your code here
   newTree.children = [];
 
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -12,14 +13,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // create new tree
-
-
-  // add child to the tree object
+  // push to the trees children array a new tree with value as parameter to new tree
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-
+  // check if value of tree currently in is target
+  if (this.value === target) {
+    return true;
+  }
+  // iterate over this.children array
+  for (let i = 0; i < this.children.length; i++) {
+    // if the child node .contains of target
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  // if none of the above success return false
+  return false;
 };
 
 /*
