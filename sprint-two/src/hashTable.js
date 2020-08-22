157dede851bdd9d8f23172a4d9b100a24186815f
ClaySamuelC <claysamuelc@gmail.com>
2020-08-22 13:19:48 -0700

    add insert and plan for retrieve in hashtable implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,21 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(index)) {
+    this._storage.set(index, [k, v]);
+  }
+  this._storage.get(index).concat([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // get to the index associated with k
+  // if (storage[index].length === 2) {
+  //   return storage[index][1];
+  // iterate over even indices of storage[index]:
+  //   if (subIndex === k):
+  //     return storage[index][subIndex+1]
+  // return value associated with key
 };
 
 HashTable.prototype.remove = function(k) {
