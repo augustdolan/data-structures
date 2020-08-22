b5063e7c0db32264cee466f35a703ce4af3ae271
ClaySamuelC <claysamuelc@gmail.com>
2020-08-22 14:05:11 -0700

    implement hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,25 +7,36 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
   if (!this._storage.get(index)) {
     this._storage.set(index, [k, v]);
+  } else {
+    for (let i = 0; i < this._storage.get(index).length; i += 2) {
+      if (this._storage.get(index)[i] === k) {
+        this._storage.get(index)[i + 1] = v;
+        return;
+      }
+    }
+    this._storage.get(index).push(k, v);
   }
-  this._storage.get(index).concat([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // get to the index associated with k
-  // if (storage[index].length === 2) {
-  //   return storage[index][1];
-  // iterate over even indices of storage[index]:
-  //   if (subIndex === k):
-  //     return storage[index][subIndex+1]
-  // return value associated with key
+  for (let i = 0; i < this._storage.get(index).length; i += 2) {
+    if (this._storage.get(index)[i] === k) {
+      return this._storage.get(index)[i+1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (let i = 0; i < this._storage.get(index).length; i += 2) {
+    if (this._storage.get(index)[i] === k) {
+      return this._storage.get(index).splice(i, 2);
+    }
+  }
 };
 
 
