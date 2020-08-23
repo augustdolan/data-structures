9fa78ee3e68d0a6370c291348905284c9b22e6a2
ClaySamuelC <claysamuelc@gmail.com>
2020-08-22 17:42:19 -0700

    Add time complexity analysis

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -51,4 +51,10 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ // insert: O(log n): Except absolute worst case structure, you do not need to travel each element to add a new one. Log n assumes the higher the n, the less likely 1 additional n will seriously impact the operation. This holds true to tree logic and statistical coin tosses.right
+
+ // contains: O(log n) ((see above))
+
+ // depthFirstLog: O(n)
  */
