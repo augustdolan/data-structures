3257a84edf68dfeb42b9c543aaca07f4794e9b27
ClaySamuelC <claysamuelc@gmail.com>
2020-08-22 17:34:41 -0700

    Implement binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,53 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  // if value lower than
+  this.left = null;
+  // if value higher than
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (value < this.value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else if (value > this.value) {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (value < this.value) {
+    if (this.left) {
+      return this.left.contains(value);
+    }
+  } else if (this.right) {
+    return this.right.contains(value);
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  // "definitely a recursive function" - Sam Clay
+  callback(this.value);
+
+  if (this.left) {
+    this.left.depthFirstLog(callback);
+  }
+
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
